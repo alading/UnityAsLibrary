11253ebca372368d1b87705a839ed4da770a9bd7
Weimin Ding <weimin.ding@gmail.com>
2023-01-16 10:02:45 -0800

    update to android x

--- mynativeapplication/src/main/java/com/unity/mynativeapp/EmptyActivity.kt
+++ mynativeapplication/src/main/java/com/unity/mynativeapp/EmptyActivity.kt
@@ -1,7 +1,8 @@
 package com.unity.mynativeapp
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class EmptyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
